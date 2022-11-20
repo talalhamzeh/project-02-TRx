7ba24176d9708f5ddb687db211d7f4875e88b0df
Michael Anderson <manderson3568@outlook.com.au>
2022-11-21 10:42:56 +1100

    indentation

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -7,39 +7,36 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className="App">
-         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/home'}>
-              TRx
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                  <Link className="nav-link" to={'/home'}>
-                    Home
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/login'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/signup'}>
-                    Sign up
-                  </Link>
-
-                </li>
-                     <li className="nav-item">
-                        <Link className="nav-link" to={'/logout'}>
-                    Logout
-                  </Link>
-
-                </li>
-              </ul>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                <div className="container">
+                <Link className="navbar-brand" to={'/home'}>
+                    TRx
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/home'}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/login'}>
+                                Login
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/signup'}>
+                                Sign up
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/logout'}>
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </div>
         </nav>
         </div>
     );
