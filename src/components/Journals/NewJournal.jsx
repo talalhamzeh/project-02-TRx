339e74528fd2b6f98f2c625c0f17be003f90082a
Michael Anderson <manderson3568@outlook.com.au>
2022-11-24 19:04:00 +1100

    added script snapshot to journal

--- src/components/Journals/NewJournal.jsx
+++ src/components/Journals/NewJournal.jsx
@@ -8,12 +8,32 @@ import { useState } from "react";
 
 
 const NewJournal = ({ toIndex }) => {
+  const [prescriptions, setPrescriptions] = useState([]);
+// list of current scripts for journal
+  const getUIDList = async () => {
+    const q = query(prescriptionsCollectionRef, where("UID", "==", currentUser.uid));
+    console.log(q);
+    const querySnapshot = await getDocs(q);
+    const qArray=[]
+    querySnapshot.forEach((doc) => {
+        qArray.push(doc.data())
+        console.log(doc.id, " => ", doc.data());
+        // 
+    });
+    console.log(qArray)
+    setPrescriptions(qArray.map((doc) => ({...doc})));
+  };
+
+
+useEffect(() => {
+    getUIDList()
+},[currentUser]);
 
   const currentUser = useAuth();
   const journalsCollectionRef = collection(db, "Journal");
  
   const createJournal = async (data) => {
-    await addDoc(journalsCollectionRef, { ...data, UID: currentUser.uid });
+    await addDoc(journalsCollectionRef, { ...data, UID: currentUser.uid, timestamp: "enter date here" });
   };
   const returnValues = (data) => {
     console.log(data);
